587c953bed941e148b8918270510190027aa4707
sstone0 <shawnrstone1995@gmail.com>
2022-01-21 15:19:01 -0500

    layout update

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -7,10 +7,6 @@ const Layout = ({ children }) => {
     <div>
       <Head>
         <title>Yelp Search</title>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Readex+Pro:wght@200;400;500;700&family=Roboto:wght@400;700&display=swap');
-        </style>
       </Head>
       <Grid className="this_is-the-grid">
         <Navbar />
